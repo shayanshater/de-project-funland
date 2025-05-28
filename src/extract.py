05e798b33f96bc10465c201ad68595cd584ffc10
Shayan <shayanshaterpoori@outlook.com>
2025-05-28 16:18:27 +0100

    Refactor our get_data_from_db function and write tests for it

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from pg8000.native import Connection, identifier, literal
+from pg8000.native import Connection, identifier, literal, DatabaseError
 from pprint import pprint
 import csv
 
@@ -16,8 +16,7 @@ def connect_to_db():
         port=int(os.getenv("totesys_port"))
     )
 
-tables_to_import = ["counterparty", "currency", "department", "design", "staff", "sales_order",
-                    "address", "payment", "purchase_order", "payment_type", "transaction"]
+
 
 def get_data_from_db(tables_to_import):
     """
@@ -31,28 +30,33 @@ def get_data_from_db(tables_to_import):
     Python dictionary
     
     """
+    
     conn = connect_to_db()
     tables_data =  {}
-
-    for table in tables_to_import:
-        table_data = conn.run(
-            f"SELECT * FROM {identifier(table)} LIMIT 2;"
-        )
-        column_names = [column['name'] for column in conn.columns]
-        formatted_tables_data = {
-            'tables' : [dict(zip(column_names, t)) for t in table_data]
-            }
-        tables_data[table] = formatted_tables_data["tables"]
-        
-    conn.close()
-
-    return tables_data
+    
+    
+    try:
+        for table in tables_to_import:
+            table_data = conn.run(
+                f"SELECT * FROM {identifier(table)} LIMIT 2;"  #WHERE {last_check} < last_updated
+            )
+            column_names = [column['name'] for column in conn.columns]
+            formatted_tables_data = {
+                'tables' : [dict(zip(column_names, t)) for t in table_data]
+                }
+            tables_data[table] = formatted_tables_data["tables"]
+        return tables_data
+    except DatabaseError as err:
+        return "database error found"
+    finally:  
+        conn.close()
 
     
-# get_data_from_db(tables_to_import)
+
+    
 
 
-def convert_data_to_csv_files():
+def convert_data_to_csv_files(tables_data):
     """
     This function will take a Python dictionary of data in multiple tables 
     (including tablename, column names within each table and the values), 
@@ -69,16 +73,31 @@ def convert_data_to_csv_files():
     returns:
     string stating what's been done: "file {filename} has been uploaded to landing bucket"
     """
-    tables_data = get_data_from_db(tables_to_import)
-    header = ['list of column names']
-    writer = csv.DictWriter(file, fieldnames=header)
-    writer.writeheader()
-    """
-    iterate through each k:v of the tables_data dict, where k is a table from totesys db.
+    #tables_data = get_data_from_db()
+    # header = ['list of column names']
+    # writer = csv.DictWriter(file, fieldnames=header)
+    # writer.writeheader()
+    # """
+    # iterate through each k:v of the tables_data dict, where k is a table from totesys db.
     
-    """
-    for values in tables_data["outer key"]["inner keys"]:
-        writer.writerow(values)
+    # """
+    # for values in tables_data["outer key"]["inner keys"]:
+    #     writer.writerow(values)
 
 
-    pass
+
+    for table in tables_data.keys():
+        header = tables_data[table][0].keys()
+        with open(f"data/extract/{table}.csv",'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=header)
+            writer.writeheader()
+            for row in tables_data[table]:
+                writer.writerow(row)
+                
+                
+
+tables_to_import = ["ounterparty", "currency", "department", "design", "staff", "sales_order",
+                    "address", "payment", "purchase_order", "payment_type", "transaction"]
+#dic = get_data_from_db()
+#convert_data_to_csv_files(dic)
+
