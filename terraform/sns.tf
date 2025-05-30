1864108f3233c057d06603dd8c3f91e3158cc87f
sapkotahari <hari.sapkota1@gmail.com>
2025-05-30 12:35:23 +0100

    sns.py and sns.tf

---
+++ terraform/sns.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "er-west-2"
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = "test-2"
+}
+
+data "aws_sns_topic" "example" {
+  name = "test-2"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = data.aws_sns_topic.example.arn
+  protocol  = "email"
+  endpoint  = "hari.sapkota1@gmail.com"
+}
